f3316afcade0dd208a3dcd786e1fc92b26a0bb24
coocoo08 <futurely08@naver.com>
2023-01-26 13:34:08 +0900

    MySQL DB연결

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ function App() {
             setMessage(data);
         });
     }, []);
-    return (<div className="App">
-        <header className="App-header"><img src={logo} className="App-logo" alt="logo"/>
-            <p> Edit <code>src/App.js</code> and save to reload. </p> <a className="App-link" href="https://reactjs.org"
-                                                                         target="_blank"
-                                                                         rel="noopener noreferrer"> Learn React </a>
-            <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul>
-        </header>
-    </div>);
+    return (
+        <div className="App">
+            <header className="App-header"><img src={logo} className="App-logo" alt="logo"/>
+                <p> Edit <code>src/App.js</code> and save to reload. </p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
+                <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
