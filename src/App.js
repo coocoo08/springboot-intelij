5aa6e4c553ed58ef7b9b1d96ea5844fe470931a7
coocoo08 <futurely08@naver.com>
2023-01-27 23:36:36 +0900

    react연결 방식 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-    const [message, setMessage] = useState([]);
+   const [hello, setHello] = useState('')
+
     useEffect(() => {
-        fetch("/hello").then((response) => {
-            return response.json();
-        }).then(function (data) {
-            setMessage(data);
-        });
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
     }, []);
+
     return (
-        <div className="App">
-            <header className="App-header"><img src={logo} className="App-logo" alt="logo"/>
-                <p> Edit <code>src/App.js</code> and save to reload. </p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
-                <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul>
-            </header>
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
         </div>
     );
 }
